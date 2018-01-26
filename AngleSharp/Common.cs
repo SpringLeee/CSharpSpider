6df91da6a2e642af8e88cadd54d37ca6155ee0d9
springleee <1028789852@qq.com>
2018-01-26 18:27:56 +0800

    submit

---
+++ AngleSharp/Common.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngleSharp
+{
+    public  static class Common
+    {
+        public static string HttpGet(string url)
+        {
+
+            try
+            {
+                Uri uri = new Uri(url);
+                HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create(uri);
+                myReq.UserAgent = "User-Agent:Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705";
+                myReq.Accept = "*/*";
+                myReq.KeepAlive = true;
+                myReq.Timeout = 5000;
+                myReq.Headers.Add("Accept-Language", "zh-cn,en-us;q=0.5");
+                HttpWebResponse result = (HttpWebResponse)myReq.GetResponse();
+                Stream receviceStream = result.GetResponseStream();
+                StreamReader readerOfStream = new StreamReader(receviceStream, System.Text.Encoding.UTF8);
+                string strHTML = readerOfStream.ReadToEnd();
+                readerOfStream.Close();
+                receviceStream.Close();
+                result.Close();
+                return strHTML;
+            }
+            catch (Exception e)
+            {
+
+                return null;
+            }
+
+            
+        }
+
+        
+
+    }
+}
---
+++ AngleSharp/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using AngleSharp.Parser.Html;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngleSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var url = "https://www.cnblogs.com/sitehome/p/";
+            
+            
+
+            for (int i = 1; i <= 200; i++)
+            {
+                var Dom = new HtmlParser().Parse(Common.HttpGet(url+i));
+
+                var listitem = Dom.QuerySelectorAll(".post_item").ToList();
+
+                foreach (var item in listitem)
+                {
+                    var Title = item.QuerySelector("h3").QuerySelector("a").TextContent;
+
+                    var href = item.QuerySelector("h3").QuerySelector("a").GetAttribute("href");
+
+                    var ddom = new HtmlParser().Parse(Common.HttpGet(href));
+
+                    var nickname = ddom.QuerySelector("#blog-news").OuterHtml;
+                    
+
+                    Console.WriteLine(Title);
+
+                    Console.WriteLine("/n/r");
+
+                    Console.WriteLine(nickname);
+                }
+
+
+            }
+
+            Console.WriteLine("处理完成");
+          
+            Console.ReadKey();
+
+
+        }
+
+    }
+}
